1b38a915c3e423fe7405eb9c6b8655fc73ff9120
Ryan Bosinger <rbosinger@gmail.com>
2024-12-19 01:13:16 -0800

    Read input file and prepare data for day 02 solution

--- day02/solution.ts
+++ day02/solution.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+const inputFile = Bun.file("input.txt");
+const inputData = await inputFile.text();
+const inputDataLines = inputData.split("\n");
+
+const reports = inputDataLines.map((line) => {
+  return line.split(" ").map((value) => parseInt(value));
+});
+
+console.log(reports);
