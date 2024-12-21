f27b6e0f9f866cc2bbb7629b1ea4a83af4f9f9d3
Ryan Bosinger <rbosinger@gmail.com>
2024-12-21 00:12:25 -0800

    Refactor to use TypeScript class OO style

--- day03/solution.ts
+++ day03/solution.ts
@@ -1,3 +1,34 @@
+abstract class Command {
+  public commandString: string;
+
+  constructor(commandString: string) {
+    this.commandString = commandString;
+  }
+
+  public abstract execute(): number;
+}
+
+class MulCommand extends Command {
+  public a: number;
+  public b: number;
+
+  constructor(commandString: string) {
+    super(commandString);
+    const mulExtract = /\d{1,3}/g;
+    const inputNumbers = commandString.match(mulExtract);
+    if (inputNumbers) {
+      this.a = parseInt(inputNumbers[0]);
+      this.b = parseInt(inputNumbers[1]);
+    } else {
+      throw new Error("Invalid command string");
+    }
+  }
+
+  public execute(): number {
+    return this.a * this.b;
+  }
+}
+
 const inputFile = Bun.file("input.txt");
 const inputData = await inputFile.text();
 
@@ -12,13 +43,10 @@ if (!regexResult) {
 let total = 0;
 
 for (const mul of regexResult) {
-  const mulExtract = /\d{1,3}/g;
-  const inputNumbers = mul.match(mulExtract);
-  if (inputNumbers) {
-    const result = parseInt(inputNumbers[0]) * parseInt(inputNumbers[1]);
-    total += result;
-    console.log("->", mul, "=", result);
-  }
+  const mulC = new MulCommand(mul);
+  const result = mulC.execute();
+  total += result;
+  console.log("->", mul, "=", result);
 }
 
 console.log("------ TOTAL ------");
