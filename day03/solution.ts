77c0863d09f2cd7fbc5de566d15fdb2f4ba0c07a
Ryan Bosinger <rbosinger@gmail.com>
2024-12-20 22:27:25 -0800

    Kick off Day 03 with a regex

--- day03/solution.ts
+++ day03/solution.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+const inputFile = Bun.file("input.txt");
+const inputData = await inputFile.text();
+
+const mulCommandRegex = /mul\(\d{0,3},\d{0,3}\)/gm;
+const regexResult = inputData.match(mulCommandRegex);
+
+if (!regexResult) {
+  console.log("Nothing found");
+  process.exit();
+}
+
+for (const mul of regexResult) {
+  console.log("->", mul);
+}
