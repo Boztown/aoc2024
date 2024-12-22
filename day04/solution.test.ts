d1b4c4f09a99c667340377074ca5b075cc4ceff9
Ryan Bosinger <rbosinger@gmail.com>
2024-12-22 11:45:50 -0800

    Got the anser, didn't need to the specific "backwards" code at all it seems they meant

--- day04/solution.test.ts
+++ day04/solution.test.ts
@@ -14,7 +14,7 @@ MAMMMXMMMM
 MXMXAXMASX
 `;
 
-test.only("solution 01 - example data", () => {
+test("solution 01 - example data", () => {
   const result = runSolution01(exampleData);
   expect(result).toBe(18);
 });
@@ -23,5 +23,5 @@ test("solution 01 - input data", async () => {
   const inputFile = Bun.file("input.txt");
   const inputData = await inputFile.text();
   const result = runSolution01(inputData);
-  expect(result).toBeUndefined();
+  expect(result).toBe(2543);
 });
--- day04/solution.ts
+++ day04/solution.ts
@@ -1,33 +1,19 @@
 import { Matrix } from "./matrix";
 
 const word = "XMAS".split("");
-const inputFile = Bun.file("input.txt");
-const inputData = await inputFile.text();
 
 export function runSolution01(data: string) {
   const m = new Matrix(parseToArray(data));
   let foundWords = 0;
   m.iteratePoints((point) => {
-    // console.log(`callback:`, point);
     const lines = m.getAllLinesFromPoint({
       point,
       count: 4,
     });
-    // console.log(lines);
     Object.entries(lines).forEach((line) => {
-      // console.log("Line:", line, "Word:", word);
       const lineString = line[1].join("");
-      const lineDir = line[0];
 
       if (lineString === word.join("")) {
-        console.log(`Found word from:`, point);
-        foundWords++;
-      }
-      if (
-        (lineString === word.reverse().join("") && lineDir === "Left") ||
-        lineDir === "Right"
-      ) {
-        console.log(`Found REVERSED from:`, point);
         foundWords++;
       }
     });
